24e09dc945cfeb45bfc203a34891ab06fcac6fb3
dickson4954 <dicksonmurithi155@gmail.com>
2024-04-02 14:16:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const op = document.querySelector('main#main')
+op.remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Dickson murithi is the champion"
